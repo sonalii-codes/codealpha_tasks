0f8a701d1dbcc4ad6fafbb010eb7f1375326c02d
sonalii-codes <sonali81205@gmail.com>
2026-06-30 21:59:23 +0530

    task 1 hangman game

---
+++ task2.py
@@ -0,0 +1,20 @@
+# A simple Chatbot 
+def chatbot():
+# Welcome message 
+    print("Chatbot: Hello!")
+    while True:
+# take input from user
+        user = input("You: ").lower().strip()
+        if user == "hello":
+            print("Chatbot: Hii!")
+        elif user == "how are you":
+            print("Chatbot: I'am fine,Thanks!")
+        elif user == "bye":    
+            print("Chatbot: Goodbye! Have a nice day")
+            break
+# If input doesn't match with predefined response
+        else:
+            print("Chatbot: Sorry! I can't understand")
+#Call the function to start the chatbot
+chatbot()                   
+
